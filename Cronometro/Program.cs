059cd91246779a312282eaac6e7197a904c84414
carlosbaldim <carlosbaldim@gmail.com>
2022-02-03 18:05:06 -0300

    commit start cronometro

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -8,6 +8,16 @@ namespace Cronometro
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+            while(currentTime != time)
+            {
+                currentTime++;
+            }
+        }
     }
 }
 
